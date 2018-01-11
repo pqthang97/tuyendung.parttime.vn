1c8d4cfdae7bac4e4d488e0eaf72bda53b488655
thecareer <jimmy@career.vn>
2018-01-11 18:45:01 +0700

    Update header-menu.php

--- header-menu.php
+++ header-menu.php
@@ -3,14 +3,14 @@
     <div class="landing_wrapper">
         <a href="#" class="landing_mobile_menu_icon js_show_mobile_menu"></a>
         <a class="landing_logo" href="<?php echo get_site_url(); ?>">
-        parttime
+        Parttime.vn
         </a>
         <ul class="landing_menu">
           <li class="landing_menu_element">
               <a class="landing_menu_link" href="<?php echo get_site_url(); ?>/areas-of-activity">Các mẫu công việc</a>
           </li>
           <li class="landing_menu_element">
-              <a class="landing_menu_link with_dropdown js_contact_link" href="#">Liên lạc</a>
+              <a class="landing_menu_link with_dropdown js_contact_link">Liên lạc</a>
               <div class="contact_popup js_contact_popup">
                 <div class="contact_popup_item">
                     <span class="contact_popup_item_heading">
@@ -32,14 +32,16 @@
                 </div>
               </div>
           </li>
+		  <?php /*
           <li class="landing_menu_element">
-              <a class="landing_menu_link" href="">Tôi muốn tìm việc</a>
+              <a class="landing_menu_link" href="//parttime.vn">Tôi muốn tìm việc</a>
+          </li> */
+		  ?>
+          <li class="landing_menu_element">
+              <a class="landing_menu_link" href="//blog.parttime.vn">Blog</a>
           </li>
           <li class="landing_menu_element">
-              <a class="landing_menu_link" href="">Đăng nhập</a>
-          </li>
-          <li class="landing_menu_element">
-              <a class="landing_menu_link_button" href="">Đặt tuyển</a>
+              <a class="landing_menu_link_button" href="http://go.parttime.vn/create-job-request">Đăng tuyển</a>
           </li>
         </ul>
     </div>
