fa0a311f9707088ebd88a0c0a119ad3eb312eb03
thecareer <jimmy@career.vn>
2018-01-14 15:45:58 +0700

    Update header.php

--- header.php
+++ header.php
@@ -12,3 +12,21 @@
   <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
 </head>
 <body <?php body_class(); ?>>
+  <script>
+window.fbAsyncInit = function() {
+FB.init({
+appId : '198438220674493',
+autoLogAppEvents : true,
+xfbml : true,
+version : 'v2.11’
+});
+};
+(function(d, s, id){
+var js, fjs = d.getElementsByTagName(s)[];
+if (d.getElementById(id)) {return;}
+js = d.createElement(s); js.id = id;
+js.src = "https://connect.facebook.net/vi_VN/sdk.js";
+fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script’, 'facebook-jssdk’));
+</script>
+<div class="fb-customerchat" page_id="690482207700261"></div>
