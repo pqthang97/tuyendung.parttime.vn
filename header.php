23ac341c68bcd0216f6f8afa2de2a8617e578f0c
thecareer <jimmy@career.vn>
2018-01-14 23:12:30 +0700

    Update header.php

--- header.php
+++ header.php
@@ -12,9 +12,3 @@
   <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
 </head>
 <body <?php body_class(); ?>>
-<script data-skip-moving="true">
-        (function(w,d,u){
-                var s=d.createElement('script');s.async=1;s.src=u+'?'+(Date.now()/60000|0);
-                var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
-        })(window,document,'https://cdn.bitrix24.com/b5880041/crm/site_button/loader_2_kcqgx0.js');
-</script>
