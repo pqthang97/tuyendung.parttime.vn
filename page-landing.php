64526a0c8051057aeb1a61d90a2700a0dbd3abb0
thecareer <jimmy@career.vn>
2018-01-15 12:53:14 +0700

    Update page-landing.php

--- page-landing.php
+++ page-landing.php
@@ -51,12 +51,10 @@ get_header();
                         </div>
                      </div>
                   </li>
-				  <?php /*
                   <li class="landing_menu_element">
-                     <a class="landing_menu_link" href="//parttime.vn">Tôi muốn tìm việc</a>
-                  </li> */
-				  ?>
-				  <li class="landing_menu_element">
+                     <a class="landing_menu_link" target="_blank" href="//parttime.vn">Tôi muốn tìm việc</a>
+                  </li>
+	          <li class="landing_menu_element">
                      <a target="_blank" class="landing_menu_link" href="//blog.parttime.vn">Blog</a>
                   </li>
                   <li class="landing_menu_element">
