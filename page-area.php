9ae65e12325d57d54e42f7c619eb8a80554caecc
Pham Quoc Thang <quocthang@career.vn>
2018-01-15 13:47:01 +0700

    fix

--- page-area.php
+++ page-area.php
@@ -127,7 +127,7 @@ include('header-menu.php');
                     </h3>
                     <div class="landing_area_of_activity_explanation for_hostelry">
                     <?php echo $post->post_content; ?><br />
-                      <a class="landing_area_of_activity_explanation_button" href="#" onClick="window.location = '<?php echo get_site_url().'/areas-of-activity?template='.$post->ID; ?>'">Request <?php echo $post->post_title; ?></a>
+                      <a class="landing_area_of_activity_explanation_button" href="#" onClick="window.location = '<?php echo get_site_url().'/areas-of-activity?template='.$post->ID; ?>'; return false;">Request <?php echo $post->post_title; ?></a>
                     </div>
                 </div>
               <?php 
